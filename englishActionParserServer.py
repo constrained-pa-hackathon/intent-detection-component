7107811106e3058292dd420362a2408d5d24cb8b
bgmoshe <barakgross5@gmail.com>
2019-06-03 12:56:00 +0300

    Deleting uneeded prints

--- englishActionParserServer.py
+++ englishActionParserServer.py
@@ -21,7 +21,6 @@ def sentence_analyzer():
     request_url = "https://%s:%d"%(IP, PORT)
     request_url = request_url+"/%s"
     if(sentence_request['action'] != " " and sentence_request['object'] != " "):
-        print(sentence_request)
         action = sentence_request['action'] 
         sentence_object =  sentence_request['object']
         request_url = request_url+"/%s"
@@ -32,12 +31,10 @@ def sentence_analyzer():
                             for field, value in sentence_request['value'].items()]
             params_string = "&".join(params)
             request_url = request_url % (params_string)
-        print(request_url)
 
     else:
         request_url = request_url % ("error")
  #   r = requests.post(url = request_url)
-  #  print(r)
     return request_url
 
 app.run()
